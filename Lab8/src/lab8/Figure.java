0764250a5b190e7340d1c619b6e77d01c23717f7
Bunijin <45909223+Bunijin@users.noreply.github.com>
2024-02-28 09:45:48 +0700

    Update Figure.java

--- Lab8/src/lab8/Figure.java
+++ Lab8/src/lab8/Figure.java
@@ -1,55 +1,55 @@
 package lab8;
 
+import java.util.ArrayList;
+
 public class Figure {
 
-    private final String name;
-    private final char color;
-    private String position;
+    protected final String name;
+    protected final boolean isWhite;
+    protected String position;
+    ArrayList<String> movableList = new ArrayList<String>();
 
-    public Figure(String name, char color, String position) {
+    public Figure(String name, boolean isWhite, String position) {
         this.name = name;
-        this.color = color;
-        this.position = position;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public char getColor() {
-        return this.color;
-    }
-
-    public String getPos() {
-        return this.position;
-    }
-    
-    public void setPos(String position) {
+        this.isWhite = isWhite;
         this.position = position;
     }
 
     // Info for display on the board
     public String getInfo() {
+        String color = (isWhite) ? "W" : "B";
         String formattedName = this.name.equalsIgnoreCase("knight") ? "Kn" : String.valueOf(this.name.charAt(0));
-        return formattedName + this.color + this.position;
+        return  color + formattedName + this.position;
     }
 
     public void move(String position, ChessBoard board) {
+    }
+
+    void updateMovableList(ChessBoard board) {
+        this.movableList.clear();
+        for (Figure[] boardX : board.board) {
+            for (Figure boardYX : boardX) {
+                if(boardYX == null || boardYX.isWhite != this.isWhite) {
+                    //this.movableList.add(this.position);
+                }
+            }
+        }
     }
 
     public void freeMove(String destination, ChessBoard board) {
 
         int currentX = this.position.charAt(0) - 'a';
         int currentY = 8 - Integer.parseInt(this.position.substring(1));
-        int destinationX = destination.charAt(0) - 'a'; 
+        int destinationX = destination.charAt(0) - 'a';
         int destinationY = 8 - Integer.parseInt(destination.substring(1));
 
         // Check if this spot is empty or isn't the same color as this piece
-        if (board.board[destinationY][destinationX] == null || board.board[destinationY][destinationX].getColor() != this.color) {
+        if (board.board[destinationY][destinationX] == null
+                || board.board[destinationY][destinationX].isWhite != this.isWhite) {
 
             if (board.board[destinationY][destinationX] != null) {
                 // Show if the piece has captured anything
-                System.out.println(this.name + " has captured " + board.board[destinationY][destinationX].getName());
+                System.out.println(this.name + " has captured " + board.board[destinationY][destinationX].name);
             } else {
                 // There's no piece on this spot
                 System.out.println(this.name + " moved successfully.");
