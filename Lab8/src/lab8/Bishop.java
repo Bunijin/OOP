98fedb101db41359d216f289b05ac3d477721db5
Bunijin <45909223+Bunijin@users.noreply.github.com>
2024-02-28 09:45:09 +0700

    Update Bishop.java

--- Lab8/src/lab8/Bishop.java
+++ Lab8/src/lab8/Bishop.java
@@ -2,57 +2,59 @@ package lab8;
 
 public class Bishop extends Figure {
 
-    public Bishop(String name, char color, String position) {
-        super(name, color, position);
+    public Bishop(String name, boolean isWhite, String position) {
+        super(name, isWhite, position);
     }
 
     @Override
     public void move(String destination, ChessBoard board) {
         // Extract current and destination coordinates
-        int currentX = this.getPos().charAt(0) - 'a';
-        int currentY = 8 - Integer.parseInt(this.getPos().substring(1));
+        int currentX = this.position.charAt(0) - 'a';
+        int currentY = 8 - Integer.parseInt(this.position.substring(1));
         int destinationX = destination.charAt(0) - 'a';
         int destinationY = 8 - Integer.parseInt(destination.substring(1));
 
-        // Check if the move is along a diagonal path
+        if (0 > destinationX || destinationX > 7 || 0 > destinationY || destinationY > 7) {
+            System.out.println("The destination is out of range.");
+            return;
+        } else if(this.position == destination) {
+            System.out.println("Cannot move to the same position!");
+            return;
+        } else if (board.board[destinationY][destinationX] != null
+                && board.board[destinationY][destinationX].isWhite == this.isWhite) {
+            System.out.println("Cannot move! Destination is occupied by a piece of the same color.");
+            return;
+        }
+        // Check if the move is along diagonal path
         if (Math.abs(currentX - destinationX) == Math.abs(currentY - destinationY)) {
             // Determine the direction of movement
             int directionX = Integer.compare(destinationX, currentX);
             int directionY = Integer.compare(destinationY, currentY);
-
             // Initialize variables for checking along the path
             int checkX = currentX + directionX;
             int checkY = currentY + directionY;
-
             // Check for obstructions along the path
             while (checkX != destinationX && checkY != destinationY) {
                 if (board.board[checkY][checkX] != null) {
-                    System.out.println("Cannot move! Path is obstructed by " + board.board[checkY][checkX].getName());
+                    System.out.println("Cannot move! Path is obstructed by " + board.board[checkY][checkX].name);
                     return;
                 }
                 checkX += directionX;
                 checkY += directionY;
             }
+            // Setting board position
+            board.board[currentY][currentX] = null;
+            board.board[destinationY][destinationX] = this;
+            this.position = destination;
 
-            // The path is clear, proceed with the move
-            if (board.board[destinationY][destinationX] == null
-                    || board.board[destinationY][destinationX].getColor() != this.getColor()) {
-                if (board.board[destinationY][destinationX] != null) {
-                    // Show if the piece has captured anything
-                    System.out.println(this.getName() + " has captured " + board.board[destinationY][destinationX].getName());
-                } else {
-                    // There's no piece on this spot
-                    System.out.println(this.getName() + " moved successfully.");
-                }
-                // Update the board and piece position
-                board.board[currentY][currentX] = null;
-                board.board[destinationY][destinationX] = this;
-                this.setPos(destination);
-            } else {
-                System.out.println("Cannot move! Destination is occupied by a piece of the same color.");
+            if (board.board[destinationY][destinationX] == null) {
+                System.out.println(this.name + " moved successfully.");
+            } else if (board.board[destinationY][destinationX].isWhite != this.isWhite) {
+                System.out.println(this.name + " has captured " + board.board[destinationY][destinationX].name);
             }
         } else {
-            System.out.println("Invalid move for a Bishop!");
+            System.out.println("Invalid move for bishop!");
+            return;
         }
     }
 }
