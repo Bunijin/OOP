febda29cf9b327a10399cd2ca5113f1273f49907
Bunijin <45909223+Bunijin@users.noreply.github.com>
2024-02-28 09:46:32 +0700

    Update Main.java

--- Lab8/src/lab8/Main.java
+++ Lab8/src/lab8/Main.java
@@ -4,11 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         ChessBoard b = new ChessBoard();
-        b.create("Bishop",'w',"e4");
-        b.create("rook",'w',"e5");
-        b.create("queen",'b', "c5");
-        b.move("c5", "d5");
-        b.move("d5","e4");
+        b.create("Bishop",true,"e4");
+        b.create("rook",true,"e5");
+        b.create("queen",false, "c5");
+        b.display();
+        b.move("e4", "e4");
+        b.display();
     }
     
 }
