5ae6bdfb8b443a81bda030955b0b5451331bae4e
Bunijin <45909223+Bunijin@users.noreply.github.com>
2024-03-22 00:05:56 +0700

    Update Pawn.java

--- Lab8/src/lab8/Pawn.java
+++ Lab8/src/lab8/Pawn.java
@@ -14,17 +14,14 @@ public class Pawn extends Figure {
         int currentY = 8 - Integer.parseInt(this.position.substring(1));
         int destX = destination.charAt(0) - 'a';
         int destY = 8 - Integer.parseInt(destination.substring(1));
-
-        if (!checkValidMove(destination, destX, destY, board)) {
+        if (checkValidMove(destination, destX, destY, board)) {
             return;
         }
-
         if (isValidMoveForPawn(destX, destY, currentX, currentY, isWhite, board)) {
             int directionX = Integer.compare(destX, currentX);
             int directionY = Integer.compare(destY, currentY);
             int checkX = currentX + directionX;
             int checkY = currentY + directionY;
-
             while (checkX != destX || checkY != destY) {
                 if (board.board[checkY][checkX] != null) {
                     System.out.println("Cannot move! Path is obstructed by " + board.board[checkY][checkX].name);
@@ -33,7 +30,6 @@ public class Pawn extends Figure {
                 checkX += directionX;
                 checkY += directionY;
             }
-
             board.board[currentY][currentX] = null;
             if (0 < destY && destY < 7) {
                 board.board[destY][destX] = this;
@@ -44,25 +40,23 @@ public class Pawn extends Figure {
                 String txt;
                 while (!created) {
                     txt = input.nextLine();
+                    created = true;
                     switch (txt.toLowerCase()) {
                         case "knight":
                             board.board[destY][destX] = new Knight("Knight", this.isWhite, this.position);
-                            created = true;
                             break;
                         case "bishop":
                             board.board[destY][destX] = new Bishop("Bishop", this.isWhite, this.position);
-                            created = true;
                             break;
                         case "rook":
                             board.board[destY][destX] = new Rook("Rook", this.isWhite, this.position);
-                            created = true;
                             break;
                         case "queen":
                             board.board[destY][destX] = new Queen("Queen", this.isWhite, this.position);
-                            created = true;
                             break;
                         default:
                             System.out.println("Invalid input! Please try again.");
+                            created = false;
                             break;
                     }
                 }
@@ -71,7 +65,6 @@ public class Pawn extends Figure {
             }
             this.position = destination;
             board.isWhiteTurn = !board.isWhiteTurn;
-
             if (board.board[destY][destX] == null) {
                 System.out.println(this.name + " moved successfully.");
             } else if (board.board[destY][destX].isWhite != this.isWhite) {
@@ -79,18 +72,26 @@ public class Pawn extends Figure {
             }
         } else {
             System.out.println("Invalid move for pawn!");
-            return;
         }
     }
 
     private boolean isValidMoveForPawn(int destX, int destY, int currentX, int currentY, boolean isWhite, ChessBoard board) {
-        if (isWhite && destX == currentX && destY - currentY == -1) return true;
-        if (!isWhite && destX == currentX && destY - currentY == 1) return true;
-        if (isWhite && destX == currentX && (destY - currentY == -2 && currentY == 6)) return true;
-        if (!isWhite && destX == currentX && (destY - currentY == 2 && currentY == 1)) return true;
-        if (isWhite && isOpponentPiece(board, destX, destY, currentX, currentY, -1)) return true;
-        if (!isWhite && isOpponentPiece(board, destX, destY, currentX, currentY, 1)) return true;
-        return false;
+        if (isWhite && destX == currentX && destY - currentY == -1) {
+            return true;
+        }
+        if (!isWhite && destX == currentX && destY - currentY == 1) {
+            return true;
+        }
+        if (isWhite && destX == currentX && (destY - currentY == -2 && currentY == 6)) {
+            return true;
+        }
+        if (!isWhite && destX == currentX && (destY - currentY == 2 && currentY == 1)) {
+            return true;
+        }
+        if (isWhite && isOpponentPiece(board, destX, destY, currentX, currentY, -1)) {
+            return true;
+        }
+        return !isWhite && isOpponentPiece(board, destX, destY, currentX, currentY, 1);
     }
 
     private boolean isOpponentPiece(ChessBoard board, int destX, int destY, int currentX, int currentY, int direction) {
